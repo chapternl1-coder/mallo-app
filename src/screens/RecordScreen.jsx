d94e46bdf4976cdf435046525bd69431dab2be49
Mallo Developer <chapternl@naver.com>
2025-12-01 09:26:37 +0900

    녹음 화면 전체 위치 위로 조정

--- src/screens/RecordScreen.jsx
+++ src/screens/RecordScreen.jsx
@@ -119,7 +119,7 @@ function RecordScreen({
   // recordState에 따라 다른 화면 렌더링
   if (recordState === 'recording' || recordState === 'idle') {
     return (
-      <div className="flex flex-col h-full bg-white relative items-center justify-start overflow-hidden pt-10">
+      <div className="flex flex-col h-full bg-white relative items-center justify-start overflow-hidden pt-[140px] pb-[120px]">
         {/* 배경 효과 - 따뜻한 크림색 파동 */}
         <div className="absolute inset-0 pointer-events-none">
           <div
